e59cbe332d0d16538fceabd2632388c6dc2c4502
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-05-20 04:04:01 +0800

    Use absolute path

--- startslave.sh
+++ startslave.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-slavename=`hostname`
+slavename=`/bin/hostname`
 
 cd /jenkins || exit 1
 while [ ! -f salve.dontstart ]
 do
-	date
+	/bin/date
 	# mirror mode, update it if there's a timestamp change on the master
-	fetch -m -o slave.jar https://jenkins.freebsd.org/jnlpJars/slave.jar
-	java -jar slave.jar -jnlpUrl https://jenkins.freebsd.org/computer/${slavename}/slave-agent.jnlp
-	sleep 30
+	/usr/bin/fetch -m -o slave.jar https://jenkins.freebsd.org/jnlpJars/slave.jar
+	/usr/local/bin/java -jar slave.jar -jnlpUrl https://jenkins.freebsd.org/computer/${slavename}/slave-agent.jnlp
+	/bin/sleep 30
 done
