0e198f61210f2ea07ba0ccb21f511d5dd6c0c0c6
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-06-01 20:11:30 +0000

    Use slave secret

--- startslave.sh
+++ startslave.sh
@@ -3,6 +3,14 @@
 slavename=`/bin/hostname`
 
 cd /usr/local/jenkins/jenkins-slave-scripts || exit 1
+
+if [ -f slave.secret ]; then
+	secret=`cat slave.secret`
+else
+	echo "no slave secret found" >&2
+	exit 1
+fi
+
 while [ ! -f salve.dontstart ]
 do
 	/bin/date
@@ -10,6 +18,7 @@ do
 	/usr/bin/fetch -m -o slave.jar http://jenkins-master.freebsd.org/jnlpJars/slave.jar
 	/usr/local/bin/java -Djava.net.preferIPv6Addresses=true \
 		-jar slave.jar \
-		-jnlpUrl http://jenkins-master.freebsd.org/computer/${slavename}/slave-agent.jnlp
+		-jnlpUrl http://jenkins-master.freebsd.org/computer/${slavename}/slave-agent.jnlp \
+		-secret ${secret}
 	/bin/sleep 30
 done
