bc4347c268a9983eecc0a44e09ce5686813f18d9
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-06-01 19:58:51 +0000

    Connect to jenkins-master

--- startslave.sh
+++ startslave.sh
@@ -7,7 +7,7 @@ while [ ! -f salve.dontstart ]
 do
 	/bin/date
 	# mirror mode, update it if there's a timestamp change on the master
-	/usr/bin/fetch -m -o slave.jar https://jenkins.freebsd.org/jnlpJars/slave.jar
-	/usr/local/bin/java -jar slave.jar -jnlpUrl https://jenkins.freebsd.org/computer/${slavename}/slave-agent.jnlp
+	/usr/bin/fetch -m -o slave.jar http://jenkins-master.freebsd.org/jnlpJars/slave.jar
+	/usr/local/bin/java -jar slave.jar -jnlpUrl http://jenkins-master.freebsd.org/computer/${slavename}/slave-agent.jnlp
 	/bin/sleep 30
 done
