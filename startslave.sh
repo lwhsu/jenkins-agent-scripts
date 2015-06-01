0e9468a4dd6ba0331141adf19334ba865cdadc9d
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-06-01 20:01:52 +0000

    Prefer IPv6

--- startslave.sh
+++ startslave.sh
@@ -8,6 +8,8 @@ do
 	/bin/date
 	# mirror mode, update it if there's a timestamp change on the master
 	/usr/bin/fetch -m -o slave.jar http://jenkins-master.freebsd.org/jnlpJars/slave.jar
-	/usr/local/bin/java -jar slave.jar -jnlpUrl http://jenkins-master.freebsd.org/computer/${slavename}/slave-agent.jnlp
+	/usr/local/bin/java -Djava.net.preferIPv6Addresses=true \
+		-jar slave.jar \
+		-jnlpUrl http://jenkins-master.freebsd.org/computer/${slavename}/slave-agent.jnlp
 	/bin/sleep 30
 done
