86da1692e2b103ea3f038c8b649a101c45f80ae7
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-06-30 16:20:06 +0800

    Extract settings to agent.conf

--- startslave.sh
+++ startslave.sh
@@ -2,26 +2,30 @@
 
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 
-mastername="jenkins.freebsd.org"
-slavename=`/bin/hostname`
-
 cd /jenkins/jenkins-slave-scripts || exit 1
 
-if [ -f slave.secret ]; then
-	secret=`cat slave.secret`
-else
-	echo "no slave secret found" >&2
-	exit 1
+if [ -r agent.conf ]; then
+	. agent.conf
 fi
+
+for i in master secret; do
+	eval v=\$$i
+	if [ -z ${v} ]; then
+		echo "${i} is not defined" >&2
+		exit 1
+	fi
+done
+
+slavename=`/bin/hostname`
 
 while [ ! -f slave.dontstart ]
 do
 	/bin/date
 	# mirror mode, update it if there's a timestamp change on the master
-	/usr/bin/fetch -m -o slave.jar https://${mastername}/jnlpJars/slave.jar
+	/usr/bin/fetch -m -o slave.jar https://${master}/jnlpJars/slave.jar
 	/usr/local/bin/java -Djava.net.preferIPv6Addresses=true \
 		-jar slave.jar \
-		-jnlpUrl https://${mastername}/computer/${slavename}/slave-agent.jnlp \
+		-jnlpUrl https://${master}/computer/${slavename}/slave-agent.jnlp \
 		-secret ${secret}
 	/bin/sleep 30
 done
